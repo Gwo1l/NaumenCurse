1db349a44a49f2dfaabdbf18f1e3cd2072575b92
Ahyamov Robert <orbitalsta@gmail.com>
2024-10-02 23:26:12 +0500

    downloaded new spring project

---
+++ src/main/java/ru/Robert/NauJava/NauJavaApplication.java
@@ -0,0 +1,13 @@
+package ru.Robert.NauJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NauJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NauJavaApplication.class, args);
+	}
+
+}
